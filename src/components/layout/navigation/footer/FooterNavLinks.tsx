b6c3421c00d386d04c3acaa89188fe8136fdfb4b
takedo <alkacirg@gmail.com>
2022-10-19 09:30:19 +0200

    fix: Imports should be sorted alphabetically

--- src/components/layout/navigation/footer/FooterNavLinks.tsx
+++ src/components/layout/navigation/footer/FooterNavLinks.tsx
@@ -1,5 +1,5 @@
-import type { Menu } from '../../../../utils/menues';
 import MaybeLink from '../../../contentful/MaybeLink';
+import type { Menu } from '../../../../utils/menues';
 
 interface IFooterNavLinks {
   navigation: Menu;
