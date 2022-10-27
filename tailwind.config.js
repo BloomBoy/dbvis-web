688ec9437bd4a975fcacf56daf5155fe4063865f
Linn Dahlgren <linn.dahlgren@pija.se>
2022-10-27 14:16:05 +0200

    Added some regexp to the tailwind config to output classes available from contentful

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,17 +37,12 @@ module.exports = {
     'text-start',
     'text-end',
     'text-center',
-    'max-w-xs',
-    'max-w-sm',
-    'max-w-md',
-    'max-w-lg',
-    'max-w-xl',
-    'max-w-2xl',
-    'max-w-3xl',
-    'max-w-4xl',
-    'max-w-5xl',
-    'max-w-6xl',
-    'max-w-7xl',
+    {
+      pattern: /max-w-.+/,
+    },
+    {
+      pattern: /-?(?:m|p)(?:x|y)-.+/,
+    },
   ],
   theme: {
     extend: {
