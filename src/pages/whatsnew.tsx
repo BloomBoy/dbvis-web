572c618e670b897e4a5a22d35fa8653075960244
markusekblad <markus.ekblad@pija.se>
2022-10-28 11:28:16 +0200

    A start on What's new page

---
+++ src/pages/whatsnew.tsx
@@ -0,0 +1,161 @@
+import { EntryFields } from 'contentful';
+import MaybeLink from 'src/components/contentful/MaybeLink';
+import RichText from 'src/components/RichText';
+
+type WhatsNewPageProps = {
+  release: {
+    name: string;
+    releaseDate: string;
+  };
+};
+
+const content = [
+  {
+    id: 'gfjkalgj',
+    navLink: {
+      title: 'SSH CONFIGURATION',
+      id: 'SSH_CONFIGURATION',
+    },
+    richText: {
+      data: {},
+      content: [
+        {
+          data: {},
+          content: [
+            {
+              data: {},
+              marks: [],
+              value: 'SSH configuration',
+              nodeType: 'text',
+            },
+          ],
+          nodeType: 'heading-2',
+        },
+        {
+          data: {},
+          content: [
+            {
+              data: {},
+              marks: [],
+              value:
+                'SSH tunnel setup has been redesigned from being defined per individual database connections to a separate feature allowing shared SSH configurations by multiple database connections. Administration is now a lot easier and the new testing feature eases the burden of understanding why a connection cannot be established.',
+              nodeType: 'text',
+            },
+          ],
+          nodeType: 'paragraph',
+        },
+      ],
+      nodeType: 'document',
+    },
+  },
+  {
+    id: 'gfherhh',
+    navLink: {
+      title: 'Image Rendering',
+      id: 'IMAGE_RENDERING',
+    },
+    richText: {
+      data: {},
+      content: [
+        {
+          data: {},
+          content: [
+            {
+              data: {},
+              marks: [],
+              value: 'Images in binary data types can now be rendered in grids',
+              nodeType: 'text',
+            },
+          ],
+          nodeType: 'heading-2',
+        },
+        {
+          data: {},
+          content: [
+            {
+              data: {},
+              marks: [],
+              value:
+                'Viewing images in binary data fields has until now only been possible by individually opening the cell. With the new support, images can be displayed as thumbnails in the grid. The size can be controlled in Tool Properties.',
+              nodeType: 'text',
+            },
+          ],
+          nodeType: 'paragraph',
+        },
+      ],
+      nodeType: 'document',
+    },
+  },
+] as const;
+
+export default function WhatsNewPage({ release }: WhatsNewPageProps) {
+  return (
+    <div className="flex flex-row">
+      <div className="w-1/4 hidden lg:block relative border-r border-dotted">
+        <div className="fixed w-1/4">
+          <div className="p-10 flex flex-col items-end">
+            <ul>
+              <li key="#link_Welcome">
+                <MaybeLink className="text-grey" href={`#link_Welcome`}>
+                  Welcome
+                </MaybeLink>
+              </li>
+              {content
+                .filter(({ navLink }) => navLink?.id && navLink?.title)
+                .map(({ navLink }) => (
+                  <li key={navLink.id}>
+                    <MaybeLink
+                      className="text-grey"
+                      href={`#link_${navLink.id}`}
+                    >
+                      {navLink.title}
+                    </MaybeLink>
+                  </li>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div id="link_Welcome" className="flex flex-col flex-1 py-20">
+        {/* Header */}
+        <div className="w-full px-10">
+          <span>
+            {release.name} released on {release.releaseDate}
+          </span>
+          <h1 className="text-xxl">{`What's new in ${release.name}`}</h1>
+        </div>
+
+        {/* Content */}
+        <hr className="border-dotted border-grey-500 my-16 opacity-20" />
+        {content.map((c) => (
+          <div
+            id={c?.navLink?.id ? `link_${c.navLink.id}` : undefined}
+            key={c.id}
+          >
+            <hr className="border-dotted border-grey-500 my-16 opacity-20" />
+            <div className="p-10">
+              {/*  */}
+              <RichText content={c.richText as any as EntryFields.RichText} />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getStaticProps(): Promise<{ props: WhatsNewPageProps }> {
+  //   ctx: GetStaticPropsContext,
+  //   GetStaticPropsResult<WithGlobals<WithCollectedData<WhatsNewPageProps>>>
+  //   const preview = ctx.preview || false;
+  return {
+    props: {
+      //   preview,
+      release: {
+        name: '14.0',
+        releaseDate: '2022-09-12',
+      },
+    },
+  };
+}
