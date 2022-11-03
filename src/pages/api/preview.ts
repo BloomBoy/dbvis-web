e21f10bc1a094d8a3ec63c0e3ae624f0a71e44d5
Linn Dahlgren <linn.dahlgren@pija.se>
2022-11-03 16:48:15 +0100

    added preview mode

--- src/pages/api/preview.ts
+++ src/pages/api/preview.ts
@@ -1,8 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import * as Contentful from 'contentful';
-import { ContentTypeFieldsMap } from 'src/utils/contentful';
-import { objectEntries, fromEntries } from 'src/utils/objects';
 import getContentfulClient from 'src/utils/getContentfulClient.mjs';
+import { isValidEntry, getEntryUrl } from 'src/utils/resolveUrl';
 
 class HttpError extends Error {
   code: number;
@@ -13,97 +11,6 @@ class HttpError extends Error {
   }
 }
 HttpError.prototype.name = HttpError.name;
-
-const simpleUrlPrefix = {
-  standardPage: '/',
-  databasePage: '/database/',
-};
-
-function makeUrlMap<
-  T extends {
-    [key in Exclude<keyof ContentTypeFieldsMap, keyof typeof simpleUrlPrefix>]:
-      | ((
-          entry: Contentful.Entry<ContentTypeFieldsMap[key]>,
-        ) => Promise<string> | string)
-      | null;
-  },
->(o: T) {
-  return o as {
-    [key in keyof T & keyof ContentTypeFieldsMap]: T[key] extends null
-      ? null
-      : (
-          entry: Contentful.Entry<ContentTypeFieldsMap[key]>,
-        ) => Promise<string> | string;
-  };
-}
-
-const typeUrlMap = makeUrlMap({
-  userReview: null,
-  reviewSource: null,
-  stringToken: () => '/',
-  menu: () => '/',
-  menuItem: () => '/',
-  extraDatabaseSearchResult: null,
-});
-
-type MapFn<T extends [keyof ContentTypeFieldsMap, string]> = [
-  T[0],
-  (
-    entry: Contentful.Entry<ContentTypeFieldsMap[T[0]]>,
-  ) => Promise<string> | string,
-];
-
-const contentTypeUrlMap = {
-  ...fromEntries(
-    objectEntries(simpleUrlPrefix).map((entry) => {
-      const ret: MapFn<typeof entry> = [
-        entry[0],
-        ({
-          fields: { slug },
-        }: {
-          readonly fields: { readonly slug: unknown };
-        }) => {
-          if (typeof slug !== 'string' || slug === '') {
-            throw new HttpError(400, 'Invalid slug on entry');
-          }
-          if (slug === '/') return entry[1];
-          return `${entry[1]}${encodeURIComponent(slug)}`;
-        },
-      ];
-      return ret;
-    }),
-  ),
-  ...typeUrlMap,
-};
-
-function isValidEntry(entry: Contentful.Entry<unknown>): entry is ValidEntry {
-  return Object.hasOwn(contentTypeUrlMap, entry.sys.contentType.sys.id);
-}
-
-function getUrl<T extends ValidEntry>(entry: T): Promise<string> | string {
-  const resolver = contentTypeUrlMap[entry.sys.contentType.sys.id] as Extract<
-    typeof typeUrlMap[keyof typeof typeUrlMap],
-    (entry: T) => Promise<string> | string
-  > | null;
-  if (resolver == null) {
-    throw new HttpError(400, 'Unsupported contentType for preview');
-  }
-  return resolver(entry);
-}
-
-type ValidEntry = {
-  [key in keyof typeof contentTypeUrlMap]: Contentful.Entry<
-    ContentTypeFieldsMap[key]
-  > & {
-    sys: {
-      contentType: {
-        sys: {
-          id: key;
-        };
-      };
-    };
-  };
-}[keyof typeof contentTypeUrlMap];
 
 export default async function preview(
   req: NextApiRequest,
@@ -124,11 +31,18 @@ export default async function preview(
   if (!isValidEntry(entry)) {
     return res.status(400).json({ message: 'Invalid contentType for entry' });
   }
+
   try {
-    const url = await getUrl(entry);
+    const url = getEntryUrl(entry) ?? '/';
 
     // Enable Preview Mode by setting the cookies
-    res.setPreviewData({ previewToken: process.env.CF_PREVIEW_ACCESS_TOKEN });
+    res.setHeader(
+      'Set-Cookie',
+      `previewToken=${encodeURIComponent(
+        process.env.CF_PREVIEW_ACCESS_TOKEN ?? '',
+      )}; path=/; SameSite=Strict`,
+    );
+    res.setPreviewData({});
 
     // Redirect to the path from the fetched post
     // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
---
+++ src/utils/resolveUrl.ts
@@ -0,0 +1,150 @@
+import { isAsset, isLink, SafeEntryFields } from './contentful';
+import * as Contentful from 'contentful';
+import { ContentTypeFieldsMap } from 'src/utils/contentful';
+import { objectEntries, fromEntries } from 'src/utils/objects';
+
+const simpleUrlPrefix = {
+  standardPage: '/',
+  databasePage: '/database/',
+};
+
+function makeUrlMap<
+  T extends {
+    [key in Exclude<keyof ContentTypeFieldsMap, keyof typeof simpleUrlPrefix>]:
+      | ((entry: Contentful.Entry<ContentTypeFieldsMap[key]>) => string)
+      | null;
+  },
+>(o: T) {
+  return o as {
+    [key in keyof T & keyof ContentTypeFieldsMap]: T[key] extends null
+      ? null
+      : (entry: Contentful.Entry<ContentTypeFieldsMap[key]>) => string;
+  };
+}
+
+const typeUrlMap = makeUrlMap({
+  userReview: null,
+  reviewSource: null,
+  stringToken: null,
+  menu: null,
+  menuItem: null,
+  extraDatabaseSearchResult: null,
+});
+
+type MapFn<T extends [keyof ContentTypeFieldsMap, string]> = [
+  T[0],
+  (entry: SafeEntryFields.Entry<ContentTypeFieldsMap[T[0]]>) => string | null,
+];
+
+const contentTypeUrlMap = {
+  ...fromEntries(
+    objectEntries(simpleUrlPrefix).map((entry) => {
+      const ret: MapFn<typeof entry> = [
+        entry[0],
+        ({
+          fields: { slug },
+        }: {
+          readonly fields: { readonly slug: unknown };
+        }) => {
+          if (typeof slug !== 'string' || slug === '') {
+            return null;
+          }
+          if (slug === '/') return entry[1];
+          return `${entry[1]}${encodeURIComponent(slug)}`;
+        },
+      ];
+      return ret;
+    }),
+  ),
+  ...typeUrlMap,
+};
+
+export function isValidEntry(
+  entry: SafeEntryFields.Entry<unknown>,
+): entry is ValidEntry {
+  return Object.hasOwn(contentTypeUrlMap, entry.sys.contentType.sys.id);
+}
+
+export function getEntryUrl<T extends ValidEntry>(entry: T): string | null {
+  const resolver = contentTypeUrlMap[entry.sys.contentType.sys.id] as Extract<
+    typeof contentTypeUrlMap[keyof typeof contentTypeUrlMap],
+    (entry: T) => string | null
+  > | null;
+  if (resolver == null) {
+    return null;
+  }
+  return resolver(entry);
+}
+
+type ValidEntry = {
+  [key in keyof typeof contentTypeUrlMap]: Contentful.Entry<
+    ContentTypeFieldsMap[key]
+  > & {
+    sys: {
+      contentType: {
+        sys: {
+          id: key;
+        };
+      };
+    };
+  };
+}[keyof typeof contentTypeUrlMap];
+
+function getUrlFromEntryOrAsset(
+  entryOrAsset: SafeEntryFields.Entry<unknown> | SafeEntryFields.Asset,
+): { href: string; title?: string; contentType?: string } | null {
+  if (isAsset(entryOrAsset)) {
+    return {
+      href: entryOrAsset.fields.file.url,
+      title: entryOrAsset.fields.title,
+      contentType: entryOrAsset.fields.file.contentType,
+    };
+  }
+  if (isValidEntry(entryOrAsset)) {
+    const href = getEntryUrl(entryOrAsset);
+    if (href == null) return null;
+    return {
+      href,
+    };
+  }
+  return null;
+}
+
+export function resolveUrl(target: string): {
+  href: string;
+  title: string;
+};
+export function resolveUrl(
+  target:
+    | string
+    | SafeEntryFields.SafeEntry<unknown>
+    | SafeEntryFields.SafeAsset
+    | undefined
+    | null,
+): { href: string; title: string } | null;
+export function resolveUrl(
+  target:
+    | string
+    | SafeEntryFields.SafeEntry<unknown>
+    | SafeEntryFields.SafeAsset
+    | undefined
+    | null,
+): { href: string; title?: string } | null {
+  if (target == null) {
+    return null;
+  }
+  if (typeof target === 'string') {
+    return {
+      href: target,
+    };
+  }
+
+  if (isLink(target)) {
+    if (target.sys.linkType === 'Asset') {
+      return null;
+    }
+    return null;
+  }
+
+  return getUrlFromEntryOrAsset(target);
+}
