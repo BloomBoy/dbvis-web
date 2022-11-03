7f16b5c20c0b19a999730fd6a7ee508191df39ed
Linn Dahlgren <linn.dahlgren@pija.se>
2022-11-03 12:58:07 +0100

    Add support for title breakpoint sizes

--- tailwindPlugins/titleComponent.js
+++ tailwindPlugins/titleComponent.js
@@ -1,126 +1,187 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const plugin = require('tailwindcss/plugin');
 
+/**
+ * @typedef {import('tailwindcss').Config} TailwindConfig
+ * @typedef {NonNullable<TailwindConfig['theme']>['fontSize'] extends import('tailwindcss/types/config').ResolvableTo<infer T> ? NonNullable<T>[string] : never} FontSize
+ */
+
 function camelToSnake(camel) {
   return camel.replace(/([A-Z])/g, (g) => `-${g[0].toLowerCase()}`);
 }
 
-function getTitleConfig(rawConf, prefix) {
+function getTitleConfig(rawConf, prefix, variableMap = { DEFAULT: {} }) {
   const titleConfig =
     typeof rawConf === 'object' && rawConf != null && !Array.isArray(rawConf)
       ? Object.fromEntries(
           Object.entries(rawConf).filter(([key]) => key !== 'DEFAULT'),
         )
       : {};
-
-  let variableMap = {};
   if (
     typeof rawConf === 'object' &&
     rawConf != null &&
     !Array.isArray(rawConf) &&
     rawConf.DEFAULT != null
   ) {
-    Object.keys(rawConf.DEFAULT).forEach((subKey) => {
+    let defaultConf;
+    let defaultExtras;
+    if (Array.isArray(rawConf.DEFAULT)) {
+      [defaultConf, defaultExtras] = rawConf.DEFAULT;
+    } else {
+      defaultConf = rawConf.DEFAULT;
+    }
+    Object.keys(defaultConf).forEach((subKey) => {
       const variable = `${prefix}-${camelToSnake(subKey)}`;
-      if (!(subKey in variableMap)) {
-        if (rawConf.DEFAULT != null && rawConf.DEFAULT[subKey] != null) {
-          variableMap[subKey] = `var(${variable},${rawConf.DEFAULT[subKey]})`;
+      if (!(subKey in variableMap.DEFAULT)) {
+        const value = defaultConf[subKey];
+        if (value != null) {
+          variableMap.DEFAULT[subKey] = `var(${variable}, ${value})`;
         } else {
-          variableMap[subKey] = `var(${variable})`;
+          variableMap.DEFAULT[subKey] = `var(${variable})`;
         }
       }
     });
+    if (defaultExtras != null) {
+      Object.entries(defaultExtras).forEach(([variant, conf]) => {
+        Object.keys(conf).forEach((subKey) => {
+          const variable = `${prefix}-${camelToSnake(subKey)}`;
+          if (!(variant in variableMap)) {
+            variableMap[variant] = {};
+          }
+          if (!(subKey in variableMap[variant])) {
+            const value = conf[subKey];
+            if (value != null) {
+              variableMap[variant][
+                subKey
+              ] = `var(${variable}_${variant},${value})`;
+            } else if (variableMap.DEFAULT[subKey] != null) {
+              variableMap[variant][
+                subKey
+              ] = `var(${variable}_${variant},${variableMap.DEFAULT[subKey]})`;
+            } else {
+              variableMap[variant][
+                subKey
+              ] = `var(${variable}_${variant},var(${variable}))`;
+            }
+          }
+        });
+      });
+    }
   }
 
-  return {
-    config: Object.fromEntries(
-      Object.entries(titleConfig).map(([key, value]) => {
-        return [
-          key,
-          Object.fromEntries(
-            Object.entries(value).map(([subKey, val]) => {
+  return Object.fromEntries(
+    Object.entries(titleConfig).map(([key, value]) => {
+      let conf, extra;
+      if (Array.isArray(value)) {
+        [conf, extra] = value;
+      } else {
+        conf = value;
+      }
+      return [
+        key,
+        Object.fromEntries([
+          ...Object.entries(conf).map(([subKey, val]) => {
+            const variable = `${prefix}-${camelToSnake(subKey)}`;
+            if (!(subKey in variableMap.DEFAULT)) {
+              variableMap.DEFAULT[subKey] = `var(${variable})`;
+            }
+            return [variable, val];
+          }),
+          ...Object.entries(extra ?? {}).flatMap(([variant, extraConf]) => {
+            if (!(variant in variableMap)) {
+              variableMap[variant] = {};
+            }
+            return Object.entries(extraConf).map(([subKey, val]) => {
               const variable = `${prefix}-${camelToSnake(subKey)}`;
-              return [variable, val];
-            }),
-          ),
-        ];
-      }),
-    ),
-    variableMap,
-  };
+              if (!(subKey in variableMap[variant])) {
+                if (variableMap.DEFAULT[subKey] != null) {
+                  variableMap[variant][
+                    subKey
+                  ] = `var(${variable}_${variant},${variableMap.DEFAULT[subKey]})`;
+                } else {
+                  variableMap[variant][
+                    subKey
+                  ] = `var(${variable}_${variant},var(${variable}))`;
+                }
+              }
+              return [`${variable}_${variant}`, val];
+            });
+          }),
+        ]),
+      ];
+    }),
+  );
 }
 
 function getTitleConfigs(
   rawMainConf,
   rawSubConf,
-  variableMap = { main: {}, sub: {} },
+  variableMap = { main: { DEFAULT: {} }, sub: { DEFAULT: {} } },
 ) {
-  const { config: subConf, variableMap: subVariableMap } = getTitleConfig(
-    rawSubConf,
-    `--title-sub`,
-  );
-  variableMap.sub = { ...variableMap.sub, ...subVariableMap };
+  const subConf = getTitleConfig(rawSubConf, `--title-sub`, variableMap.sub);
 
-  const { config: mainConf, variableMap: mainVariableMap } = getTitleConfig(
+  const mainConf = getTitleConfig(
     rawMainConf,
     `--title-main`,
-  );
-  variableMap.main = { ...variableMap.main, ...mainVariableMap };
-
-  const mergedConfs = Object.fromEntries(
-    [...Object.keys(subConf), ...Object.keys(mainConf)].map((key) => {
-      return [key, { ...(mainConf[key] ?? null), ...(subConf[key] ?? null) }];
-    }),
+    variableMap.main,
   );
 
   return {
     mainConf,
     subConf,
-    mergedConfs,
     variableMap,
   };
 }
 
+/**
+ * @param {FontSize | undefined | null} fontSize
+ * @returns {{fontSize?:string;lineHeight?:string;letterSpacing?:string;fontWeight?:string|number;} | null}
+ */
+function convertFontSize(fontSize) {
+  if (fontSize == null) return null;
+  /** @type {{fontSize?:string;lineHeight?:string;letterSpacing?:string;fontWeight?:string|number;}} */
+  const config = {};
+  if (Array.isArray(fontSize)) {
+    if (typeof fontSize[1] === 'string') {
+      config.lineHeight = fontSize[1];
+    } else {
+      Object.assign(config, fontSize[1]);
+    }
+    config.fontSize = fontSize[0];
+  } else {
+    config.fontSize = fontSize;
+  }
+  return config;
+}
+
 module.exports = plugin(
-  function titleHelper({ matchUtilities, addUtilities, theme }) {
-    const variableMap = { main: {}, sub: {} };
-    const {
-      mainConf: mainFontConf,
-      subConf: subFontConf,
-      mergedConfs: mergedFontConfs,
-    } = getTitleConfigs(
+  function titleHelper({ matchUtilities, addUtilities, e, theme }) {
+    const variableMap = { main: { DEFAULT: {} }, sub: { DEFAULT: {} } };
+    const { mainConf: mainFontConf, subConf: subFontConf } = getTitleConfigs(
       theme('title.main.font'),
       theme('title.sub.font'),
       variableMap,
     );
-    const {
-      mainConf: maincolorConf,
-      subConf: subColorConf,
-      mergedConfs: mergedColorConfs,
-    } = getTitleConfigs(
+    const { mainConf: maincolorConf, subConf: subColorConf } = getTitleConfigs(
       theme('title.main.color'),
       theme('title.sub.color'),
       variableMap,
     );
     matchUtilities(
       {
-        title: (value) => value,
-      },
-      {
-        values: mergedFontConfs,
-      },
-    );
-    matchUtilities(
-      {
-        title: (value) => value,
-      },
-      {
-        values: mergedColorConfs,
-      },
-    );
-    matchUtilities(
-      {
-        'title-sub': (value) => value,
+        'title-sub': (value) => ({
+          ...Object.fromEntries(
+            Object.keys(variableMap.sub)
+              .filter((key) => key !== 'DEFAULT')
+              .flatMap((variant) => {
+                return Object.entries(value).map(([key, val]) => [
+                  `${key}_${variant}`,
+                  val,
+                ]);
+              }),
+          ),
+          ...value,
+        }),
       },
       {
         values: subFontConf,
@@ -128,7 +189,19 @@ module.exports = plugin(
     );
     matchUtilities(
       {
-        'title-main': (value) => value,
+        title: (value) => ({
+          ...Object.fromEntries(
+            Object.keys(variableMap.main)
+              .filter((key) => key !== 'DEFAULT')
+              .flatMap((variant) => {
+                return Object.entries(value).map(([key, val]) => [
+                  `${key}_${variant}`,
+                  val,
+                ]);
+              }),
+          ),
+          ...value,
+        }),
       },
       {
         values: mainFontConf,
@@ -136,7 +209,19 @@ module.exports = plugin(
     );
     matchUtilities(
       {
-        'title-sub': (value) => value,
+        'title-sub': (value) => ({
+          ...Object.fromEntries(
+            Object.keys(variableMap.sub)
+              .filter((key) => key !== 'DEFAULT')
+              .flatMap((variant) => {
+                return Object.entries(value).map(([key, val]) => [
+                  `${key}_${variant}`,
+                  val,
+                ]);
+              }),
+          ),
+          ...value,
+        }),
       },
       {
         values: subColorConf,
@@ -144,50 +229,69 @@ module.exports = plugin(
     );
     matchUtilities(
       {
-        'title-main': (value) => value,
+        title: (value) => ({
+          ...Object.fromEntries(
+            Object.keys(variableMap.main)
+              .filter((key) => key !== 'DEFAULT')
+              .flatMap((variant) => {
+                return Object.entries(value).map(([key, val]) => [
+                  `${key}_${variant}`,
+                  val,
+                ]);
+              }),
+          ),
+          ...value,
+        }),
       },
       {
         values: maincolorConf,
       },
     );
-    addUtilities({
-      '.title-main': {
-        ...variableMap.main,
-      },
-      '.title-sub': {
-        ...variableMap.sub,
-      },
-    });
+    const screens = theme('screens');
+    const variantKeys = ['DEFAULT', ...Object.keys(screens)];
+    addUtilities(
+      Object.fromEntries(
+        Object.entries(variableMap).flatMap(([type, conf]) => {
+          return variantKeys
+            .map((variant) => {
+              const subConf = conf[variant];
+              if (subConf == null) return null;
+              if (variant === 'DEFAULT') {
+                return [`.${e(`title-${type}`)}`, subConf];
+              } else if (variant in screens) {
+                return [
+                  `@media (min-width: ${screens[variant]})`,
+                  {
+                    [`.${e(`title-${type}`)}`]: subConf,
+                  },
+                ];
+              }
+            })
+            .filter((entry) => entry != null);
+        }),
+      ),
+    );
   },
   {
     theme: {
       title: ({ theme }) => ({
         main: {
-          font: Object.fromEntries(
-            [
-              ...Object.entries(theme('fontSize')),
-              ['DEFAULT', theme('fontSize')['7xl']],
-            ].map(
-              /**
-               * @param {[string, FontSize | undefined]} entry
-               */
-              ([key, val]) => {
-                /** @type {{fontSize?:string;lineHeight?:string;letterSpacing?:string;fontWeight?:string|number;}} */
-                const config = {};
-                if (Array.isArray(val)) {
-                  if (typeof val[1] === 'string') {
-                    config.lineHeight = val[1];
-                  } else {
-                    Object.assign(config, val[1]);
-                  }
-                  config.fontSize = val[0];
-                } else {
-                  config.fontSize = val;
-                }
-                return [key, config];
-              },
+          font: {
+            ...Object.fromEntries(
+              Object.entries(theme('fontSize')).map(
+                /**
+                 * @param {[string, FontSize | undefined]} entry
+                 */
+                ([key, val]) => {
+                  return [key, convertFontSize(val)];
+                },
+              ),
             ),
-          ),
+            DEFAULT: [
+              convertFontSize(theme('fontSize.5xl')),
+              { md: convertFontSize(theme('fontSize.7xl')) },
+            ],
+          },
           color: {
             DEFAULT: {
               color: theme('colors.gray.900'),
